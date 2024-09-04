b6344488f0ed7b0551791eded4f103c7e315b816
SoloEinstein ElonJames MIT <solomoneinsteinmit300@gmail.com>
2024-09-04 14:56:58 +0000

    Worked optimising NavLinks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import Dashboard from "./pages/Dashboard";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import PreviousData from "./pages/PreviousData";
 import CheckVitals from "./pages/CheckVitals";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   const router = createBrowserRouter(
@@ -19,6 +21,9 @@ function App() {
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="data" element={<PreviousData />} />
         <Route path="check_vitals" element={<CheckVitals />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="logout" element={<Login />} />
         {/* <Route path="message_assistu" element={<ChatScreen />} /> */}
       </Route>
     )
--- client/src/pages/Partials/LeftNav.jsx
+++ client/src/pages/Partials/LeftNav.jsx
@@ -6,33 +6,60 @@ import { IoSettings } from "react-icons/io5";
 import { FaRobot } from "react-icons/fa6";
 import { TbHealthRecognition } from "react-icons/tb";
 import NavLinkCard from "../../components/NavLinkCard";
+import { IoLogOut } from "react-icons/io5";
 
 const LeftNav = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      icon: <TbLayoutDashboardFilled />,
+      to: "/",
+    },
+    {
+      label: "Previous Data",
+      icon: <BsFillBarChartLineFill />,
+      to: "/data",
+    },
+    {
+      label: "Check Vitals",
+      icon: <TbHealthRecognition className="text-2xl" />,
+      to: "/check_vitals",
+    },
+    {
+      label: "FitFlow AI",
+      icon: <FaRobot className="text-2xl" />,
+      to: "/3",
+    },
+    // {
+    //   label: "Settings",
+    //   icon: <IoSettings />,
+    //   to: "/4",
+    // },
+  ];
   return (
-    <div className=" w-16 bg-primary flex flex-col gap-10 items-center py-5">
-      <div className="text-primary text-lg font-extrabold bg-white p-2 rounded-medium w-10 h-10 flex items-center justify-center ">
-        <span className="pt-1">Fit</span>
+    <div className=" w-16 bg-primary flex flex-col justify-between gap-10 items-center py-5">
+      <div className="flex flex-col gap-10">
+        <div className="text-primary text-lg font-extrabold bg-white p-2 rounded-medium w-10 h-10 flex items-center justify-center ">
+          <span className="pt-1">Fit</span>
+        </div>
+        {/*// TODO: OPTIMIZE BUTTON CODE */}
+        <div className="flex flex-col gap-8">
+          {links.map((link) => (
+            <NavLinkCard
+              key={link.label}
+              tooltip={link.label}
+              icon={link.icon}
+              to={link.to}
+            />
+          ))}
+        </div>
       </div>
-      {/*// TODO: OPTIMIZE BUTTON CODE */}
-      <div className="flex flex-col gap-8">
-        <NavLinkCard
-          icon={<TbLayoutDashboardFilled />}
-          to="/"
-          tooltip="Dashboard"
-        />
-        <NavLinkCard
-          icon={<BsFillBarChartLineFill />}
-          to="/data"
-          tooltip="Previous Data"
-        />
-        <NavLinkCard
-          icon={<TbHealthRecognition className="text-xl" />}
-          to="/check_vitals"
-          tooltip="Check Vitals"
-        />
-        <NavLinkCard icon={<FaRobot />} to="/3" tooltip="FitFlow AI" />
-        {/* <NavLinkCard icon={<IoSettings />} to="/2" /> */}
-      </div>
+
+      <NavLinkCard
+        icon={<IoLogOut className="text-2xl" />}
+        to="/logout"
+        tooltip="Logout"
+      />
     </div>
   );
 };
